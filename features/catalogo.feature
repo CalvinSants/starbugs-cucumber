e36a6985921f15894e0f99c2c248bf58bac42a27
Lanna Kamaitachi <alannaestudante@gmail.com>
2025-02-18 00:42:50 -0300

    automacao-de-compra-de-cafe

--- features/catalogo.feature
+++ features/catalogo.feature
@@ -7,4 +7,19 @@ Scenario:  acessar catalogo de cafes na página principal
     When acesso a página principal da starbugs
     Then eu devo ver uma lista de cafes disponíveis
 
-    
+Scenario: iniciar a compra de um café
+
+    Given que estou na página principal da Starbugs
+        And que desejo comprar o café "Expresso Gelado"
+        And que esse produto custa "R$ 9,99"
+        And que o custo de entrega é de "R$ 10,00"
+    When inicio a compra desse item
+    Then devo ver a pagina de checkout com os detalhes do produto
+        And o valor total da compra deve ser de "R$ 19,99"
+        
+Scenario: cafe indisponivel
+
+    Given que estou na página principal da Starbugs
+        And que desejo comprar o café "Expresso Cremoso"
+    When inicio a compra desse item
+    Then devo ver um popup informando que o produto esta indisponivel
--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -6,3 +6,49 @@ Then('eu devo ver uma lista de cafes disponíveis') do
     products = all('.coffee-item')
     expect(products.size).to be > 0
 end
+
+ 
+
+Given('que estou na página principal da Starbugs') do
+    visit 'https://starbugs-qa.vercel.app/'
+end
+
+Given('que desejo comprar o café {string}') do |product_name|
+    @product_name = product_name
+end
+
+Given('que esse produto custa {string}') do |product_price|
+    @product_price = product_price
+end
+
+Given('que o custo de entrega é de {string}') do |delivery_price|
+    @delivery_price = delivery_price
+end
+
+When('inicio a compra desse item') do
+    product = find('.coffee-item', text: @product_name)
+    product.find('.buy-coffee').click
+end
+
+Then('devo ver a pagina de checkout com os detalhes do produto') do
+    product_title = find('.item-details h1')
+    expect(product_title.text).to eql @product_name
+
+    sub_price = find('.subtotal .sub-price')
+    expect(sub_price.text).to eql @product_price
+
+    delivery_sub_price = find('.delivery .delivery-price')
+    expect(delivery_sub_price.text).to eql @delivery_price
+end
+
+Then('o valor total da compra deve ser de {string}') do |total|
+    total_price = find('.total .total-price')
+    expect(total_price.text). to eql total
+end
+
+
+
+Then('devo ver um popup informando que o produto esta indisponivel') do
+    popup = find('.swal2-html-container')
+    expect(popup.text).to eql 'Produto indisponível'
+end
